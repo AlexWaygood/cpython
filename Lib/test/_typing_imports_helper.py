9745e3dee45f25b521c8d442e8f144978b15d33d
Alex Waygood <Alex.Waygood@Gmail.com>
2021-10-22 11:11:56 +0100

    MRE seems hard!

--- Lib/test/_typing_imports_helper.py
+++ Lib/test/_typing_imports_helper.py
@@ -1,16 +1,18 @@
 from __future__ import annotations
 
-from typing import Optional, NamedTuple
+from typing import Optional, NamedTuple, TypedDict
 from dataclasses import dataclass
+from collections import OrderedDict
 
-OptionalIntType = Optional[int]
+class FooTypedDict(TypedDict):
+    a: OrderedDict
 
 class FooNamedTuple(NamedTuple):
-    a: OptionalIntType
+    a: OrderedDict
 
 @dataclass
 class FooDataclass:
-    a: OptionalIntType
+    a: OrderedDict
 
 class FooStandardClass:
-    def __init__(self, a: OptionalIntType) -> None: ...
+    def __init__(self, a: OrderedDict) -> None: ...
--- Lib/test/test_future_annotations.py
+++ Lib/test/test_future_annotations.py
@@ -1,70 +1,92 @@
 from __future__ import annotations
 
+import sys
 import unittest
 
-from dataclasses import dataclass
 from test import _typing_imports_helper
-import typing
-from typing import get_type_hints, NamedTuple
+from typing import get_type_hints
 
-class TestFutureAnnotations(unittest.TestCase):
+class TestNoExceptionsRaised(unittest.TestCase):
+    def test_typed_dict(self):
+        get_type_hints(_typing_imports_helper.FooTypedDict)
+    
     def test_named_tuple(self):
-        class IdenticalNamedTuple(NamedTuple):
-            a: typing.Optional[int]
-
-        self.assertEqual(
-            get_type_hints(_typing_imports_helper.FooNamedTuple),
-            get_type_hints(IdenticalNamedTuple)
-        )
-
-        self.assertEqual(
-            get_type_hints(_typing_imports_helper.FooNamedTuple.__init__),
-            get_type_hints(IdenticalNamedTuple.__init__)
-        )
-
-        self.assertEqual(
-            get_type_hints(_typing_imports_helper.FooNamedTuple.__new__),
-            get_type_hints(IdenticalNamedTuple.__new__)
-        )
+        get_type_hints(_typing_imports_helper.FooNamedTuple)
+        get_type_hints(_typing_imports_helper.FooNamedTuple.__new__)
 
     def test_dataclass(self):
-        @dataclass
-        class IdenticalDataclass:
-            a: typing.Optional[int]
-
-        self.assertEqual(
-            get_type_hints(_typing_imports_helper.FooDataclass),
-            get_type_hints(IdenticalDataclass)
-        )
-
-        self.assertEqual(
-            get_type_hints(_typing_imports_helper.FooDataclass.__init__),
-            get_type_hints(IdenticalDataclass.__init__)
-        )
-
-        self.assertEqual(
-            get_type_hints(_typing_imports_helper.FooDataclass.__new__),
-            get_type_hints(IdenticalDataclass.__new__)
-        )
+        get_type_hints(_typing_imports_helper.FooDataclass)
+        get_type_hints(_typing_imports_helper.FooDataclass.__new__)
+        get_type_hints(_typing_imports_helper.FooDataclass.__init__)
 
     def test_standard_class(self):
-        class IdenticalStandardClass:
-            def __init__(self, a: typing.Optional[int]) -> None: ...
+        get_type_hints(_typing_imports_helper.FooStandardClass)
+        get_type_hints(_typing_imports_helper.FooStandardClass.__new__)
+        get_type_hints(_typing_imports_helper.FooStandardClass.__init__)
 
-        self.assertEqual(
-            get_type_hints(_typing_imports_helper.FooStandardClass),
-            get_type_hints(IdenticalStandardClass)
-        )
 
-        self.assertEqual(
-            get_type_hints(_typing_imports_helper.FooStandardClass.__init__),
-            get_type_hints(IdenticalStandardClass.__init__)
-        )
+class AnnotationsOutputTest(unittest.TestCase):
+    """Base class for several below tests"""
+    def tearDown(self):
+        sys.modules.pop('collections', None)
 
-        self.assertEqual(
-            get_type_hints(_typing_imports_helper.FooStandardClass.__new__),
-            get_type_hints(IdenticalStandardClass.__new__)
-        )
+
+class TestTypedDictOutput(AnnotationsOutputTest):
+    def test_typed_dict(self):
+        annotations = get_type_hints(_typing_imports_helper.FooTypedDict)
+        from collections import OrderedDict
+        expected_annotations = {'a': OrderedDict}
+        self.assertEqual(annotations, expected_annotations)
+
+
+class TestNamedTupleOutput(AnnotationsOutputTest):
+    def test_named_tuple(self):
+        annotations = get_type_hints(_typing_imports_helper.FooNamedTuple)
+        from collections import OrderedDict
+        expected_annotations = {'a': OrderedDict}
+        self.assertEqual(annotations, expected_annotations)
+
+    def test_named_tuple_new(self):
+        annotations = get_type_hints(_typing_imports_helper.FooNamedTuple.__new__)
+        from collections import OrderedDict
+        expected_annotations = {'a': OrderedDict}
+        self.assertEqual(annotations, expected_annotations)
+    
+
+class TestDataclassOutput(AnnotationsOutputTest):
+    def test_dataclass(self):
+        annotations = get_type_hints(_typing_imports_helper.FooDataclass)
+        from collections import OrderedDict
+        expected_annotations = {'a': OrderedDict}
+        self.assertEqual(annotations, expected_annotations)
+
+    def test_dataclass_new(self):
+        annotations = get_type_hints(_typing_imports_helper.FooDataclass.__new__)
+        self.assertEqual(annotations, {})
+
+    def test_dataclass_init(self):
+        annotations = get_type_hints(_typing_imports_helper.FooDataclass.__init__)
+        from collections import OrderedDict
+        expected_annotations = {'a': OrderedDict, 'return': type(None)}
+        self.assertEqual(annotations, expected_annotations)
+
+
+class TestStandardClassOutput(AnnotationsOutputTest):
+    def test_standard_class(self):
+        annotations = get_type_hints(_typing_imports_helper.FooStandardClass)
+        self.assertEqual(annotations, {})
+
+    def test_standard_class_new(self):
+        annotations = get_type_hints(_typing_imports_helper.FooStandardClass.__new__)
+        self.assertEqual(annotations, {})
+
+    def test_standard_class_init(self):
+        annotations = get_type_hints(_typing_imports_helper.FooStandardClass.__init__)
+        from collections import OrderedDict
+        expected_annotations = {'a': OrderedDict, 'return': type(None)}
+        self.assertEqual(annotations, expected_annotations)
+
+    
 
 
 if __name__ == '__main__':
